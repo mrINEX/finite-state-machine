825b3aca9ad78c83ba6445f2d1de559cb7cb5689
mrinex <mrinex@mail.ru>
2019-10-08 21:52:44 +0300

    feat: add task

--- src/fsm.js
+++ src/fsm.js
@@ -3,30 +3,63 @@ class FSM {
      * Creates new FSM instance.
      * @param config
      */
-    constructor(config) {}
+    constructor(config) {
+        if(config === undefined)
+            throw new name('Error');
+        this.getSt = 'normal';
+        this.historytrigger = ['normal'];
+        this.eve=[];
+        this.historydel = [];
+        this.und = 0;
+        this.red = 0;
+    }
 
     /**
      * Returns active state.
      * @returns {String}
      */
-    getState() {}
+    getState() {
+        return this.getSt;
+    }
 
     /**
      * Goes to specified state.
      * @param state
      */
-    changeState(state) {}
+    changeState(state) {
+        if(state !== 'normal' && state !== 'hungry' && state !== 'busy' && state !=='sleeping')
+            throw new eer('Error');
+        else
+            {this.eve.push(state); this.historytrigger.push(this.getSt); this.getSt = state;} //this.change = this.getSt; 
+    }
 
     /**
      * Changes state according to event transition rules.
      * @param event
      */
-    trigger(event) {}
+    trigger(event) {
+            
+        if(event === 'study' && this.getSt === 'normal')
+            {this.getSt = 'busy'; this.historytrigger.push('busy'); this.eve.push('study');}
+        else if(event === 'get_tired' && this.getSt === 'busy')
+            {this.getSt = 'sleeping'; this.historytrigger.push('sleeping');this.eve.push('get_tired');}
+        else if(event === 'get_hungry' && this.getSt === 'busy' || event === 'get_hungry' && this.getSt === 'sleeping')
+            {this.getSt = 'hungry'; this.historytrigger.push('hungry');this.eve.push('get_hungry');} ////
+        else if(event === 'eat' && this.getSt === 'hungry')
+            {this.getSt = 'normal'; this.historytrigger.push('normal');this.eve.push('eat');}
+        else if(event === 'get_up' && this.getSt === 'sleeping')
+            {this.getSt = 'normal'; this.historytrigger.push('normal');this.eve.push('get_up');}
+        else
+            throw new name('Error');
+    }
 
     /**
      * Resets FSM state to initial.
      */
-    reset() {}
+    reset() {
+        if(this.getSt !== 'normal')
+            this.getSt = 'normal';
+    }
 
     /**
      * Returns an array of states for which there are specified event transition rules.
@@ -34,26 +67,63 @@ class FSM {
      * @param event
      * @returns {Array}
      */
-    getStates(event) {}
+    getStates(event) {
+        if(event === undefined)
+            return ['normal', 'busy', 'hungry', 'sleeping'];
+        if(event === 'get_hungry')
+            return ['busy', 'sleeping'];
+        if(event === 'study')
+            return ['normal'];
+        if(event === 'get_tired')
+            return ['busy'];
+        if(event === 'eat')
+            return ['hungry'];
+        if(event === 'get_up')
+            return ['sleeping'];
+        else
+            return [];
+    }
 
     /**
      * Goes back to previous state.
      * Returns false if undo is not available.
      * @returns {Boolean}
      */
-    undo() {}
+    undo() {
+        this.und += 1;
+        if(this.getSt === 'normal')
+            return false;
+        
+        console.log('(undo) getSt ^: ['+this.getSt+'] his: '+this.historytrigger);
+        this.getSt = this.historytrigger[this.historytrigger.length-2];
+        console.log('(undo) getSt _: ['+this.getSt+'] his: '+this.historytrigger);
+        return true;
+    }
 
     /**
      * Goes redo to state.
      * Returns false if redo is not available.
      * @returns {Boolean}
      */
-    redo() {}
+    redo() {
+        this.red += 1;
+        if(this.red === this.und)
+        {
+            console.log('(redo) getSt ^: ['+this.getSt+'] his: '+this.historytrigger);
+            this.getSt = this.historytrigger[this.historytrigger.length-1];
+            console.log('(redo) getSt _: ['+this.getSt+'] his: '+this.historytrigger);
+            return true;
+        }
+        if(this.getSt === 'normal')
+            return false;
+    }
 
     /**
      * Clears transition history
      */
-    clearHistory() {}
+    clearHistory() {
+        this.getSt = 'normal';
+    }
 }
 
 module.exports = FSM;
